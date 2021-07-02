1a6828c5ebcceb8b1e6cccc92619d55c57d804f3
ShortFir <shortfir@protonmail.com>
2021-07-02 14:42:45 +1000

    Input does some checking, still needs work.

--- lib/main.rb
+++ lib/main.rb
@@ -30,7 +30,9 @@ class Play
   def knight_moving_display
     show_board
     start, finish = user_input
-    display_moves(start, finish)
+    moves = display_moves(start, finish)
+    puts
+    moves.each { |move| print "#{move}\n" }
     print "\n", 'All done!', "\n", 'Press enter to exit.'
     gets
     clear_screen
@@ -44,14 +46,39 @@ class Play
       sleep 0.5
       @board.move_piece(ele, moves[index + 1])
     end
+    moves
   end
 
   def user_input
-    print "\n", 'Start  Pos (2 Int):'
-    start = gets.chomp.split('').map(&:to_i)
-    print 'Finish Pos (2 Int):'
-    finish = gets.chomp.split('').map(&:to_i)
+    print "\n", 'Horizontal then Vertical.', "\n"
+    print 'Input coordinates thusly: 00 or 56 or 77 etc.', "\n\n"
+    print 'Start  Position:'
+    start = return_input
+    print 'Finish Position:'
+    finish = return_input
     [start, finish]
+  end
+
+  # TODO: letters convert to 0, so need to redo this section
+  def return_input
+    input = []
+    loop do
+      input = gets.chomp.split('').map(&:to_i)
+      break if verify_input(input)
+
+      print 'Enter two seperate digits from 0 to 7:'
+    end
+    input
+  end
+
+  def verify_input(input)
+    return false unless input.length == 2
+    return false unless input[0] >= 0
+    return false unless input[0] <= 7
+    return false unless input[1] >= 0
+    return false unless input[1] <= 7
+
+    true
   end
 
   def fill_board(piece)
